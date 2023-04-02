c31ecf0436e15668131cd1fa24c4dc827d69ecb5
Alexander Valencia <alex16jpv@gmail.com>
2023-04-02 00:53:27 -0500

    feat: add origin * for cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: false,
+    origin: "*",
   });
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
