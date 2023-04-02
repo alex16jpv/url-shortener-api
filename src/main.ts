acc16a58b0fe58af150d76ebeee813cb71bde8a3
Alexander Valencia <alex16jpv@gmail.com>
2023-04-02 00:01:22 -0500

    feat: add wnv vars and log when the project starts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    if (process.env.STAGE === 'local') {
+      console.log('Application is running on: http://localhost:3000');
+    } else {
+      console.log('Application is running');
+    }
+  });
 }
 bootstrap();
